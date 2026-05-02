ad52cd3d4391d86953c35fed2aa93557c6b3a09d
Davi Santana <davisantanadev@gmail.com>
2026-05-02 19:25:52 -0300

    Criação da tela de Login e a Estrutura de Pastas para o ambiente de desenvolvimento.

---
+++ backend/src/server.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const app = require('./src/app')
+const connectDB = require('./src/config/db')
+
+const PORT = process.env.PORT || 3000
+
+connectDB()
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
